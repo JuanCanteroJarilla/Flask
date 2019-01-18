2ba340e45499b59549f238d7dafd551b46b11b1b
Juan <juan.cantero.jarilla@gmail.com>
2019-01-18 19:28:11 +0100

    Forms y Csrf

--- forms.py
+++ forms.py
@@ -1,8 +1,18 @@
-from wtforms import Form, StringField, TextField
+from wtforms import Form, StringField, TextField, validators, HiddenField
 from wtforms.fields.html5 import EmailField
 
+def length_honeypot(form, field):
+    #Si nos atacan, llenarán el campo oculto del formulario
+    #Si es así, se devolverá un error.
+    if len(field.data) >0:
+        raise validators.ValidationError('El campo debe estar vacio.')
 
 class CommentForm(Form):
-    username = StringField('username')
-    email = EmailField('Correo electronico')
+    username = StringField('username', [validators.length(min=4,max=25, message='Ingresa un Username válido')])
+    email = EmailField('Correo electronico',
+            [validators.Required(message='El campo email no puede estar vacio'),
+            validators.Email(message='Introduce un email válido')])
     comment = TextField('Comentario')
+    # Honeypot es un "señuelo" para futuros ataques de spam,
+    # el campo no va a estar visible para un usuario normal
+    honeypot = HiddenField('', [length_honeypot])
--- index.py
+++ index.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+from flask_wtf import CSRFProtect
+import forms
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+csrf = CSRFProtect(app)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    comment_form = forms.CommentForm(request.form)
+    if request.method == 'POST' and comment_form.validate():
+        print(comment_form.username.data)
+        print(comment_form.email.data)
+        print(comment_form.comment.data)
+    else:
+        print('Error en el formulario')
+    return render_template('index.html', form = comment_form)
 
-@app.route('/about')
+@app.route('/about', methods = ['GET', 'POST'])
 def about():
     return render_template('about.html')
 
-@app.route('/user/<name>')
+@app.route('/user/<name>', methods = ['GET', 'POST'])
 def user(name='Juan'): #Va a recoger el valor del string entre los diamantes, en el decorador
     age = 32
     my_list = ['Estrella','Leffe','Grimbergen','Paulaner']
     return render_template('user.html', nombre=name, edad=age, list=my_list)#Va a devolver el template indicado, junto con el valor del segundo parámetro
 
-@app.route('/json')
+@app.route('/json', methods = ['GET', 'POST'])
 def json():
     persons = [{
         "name" : "Juan",
